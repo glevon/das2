c637f45779e667e0b773c66bc446dc05080a18be
glevon <levon77777@gmai.com>
2020-05-06 02:13:07 +0400

    Updates

---
+++ precache-manifest.527018f8688b37ae98db1bdcd87aac79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc46d1da277947671499fb2600b0b64",
+    "url": "/das2/index.html"
+  },
+  {
+    "revision": "b1adbcb333c3b6274211",
+    "url": "/das2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33cf4a5a98b8ee4c63ff",
+    "url": "/das2/static/js/2.588e5f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/das2/static/js/2.588e5f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1adbcb333c3b6274211",
+    "url": "/das2/static/js/main.53cf4b98.chunk.js"
+  },
+  {
+    "revision": "2c33383e58ea056f9bba",
+    "url": "/das2/static/js/runtime-main.f3b43418.js"
+  }
+]);
